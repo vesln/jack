816dd86df3973d96b6ec23cd3da79b13fa230880
Veselin Todorov <veselin@veselin.bg>
2012-02-01 09:25:18 +0200

    Adding basic expectations.

--- test/stub.test.js
+++ test/stub.test.js
@@ -18,25 +18,61 @@ var jack = require('../lib');
  */
 var Stub = require('../lib/stub');
 
+/**
+ * Dummy function used in tests.
+ * 
+ * @type {Function}
+ */
+var foo = function() { 
+  return 4; 
+}
+
 describe('Stub', function() {
-  describe('andReturn', function() {
+  describe('.andReturn()', function() {
     it('should return a supplied value when called.', function() {
       var obj = new EventEmitter;
       obj.stub('on').andReturn(3);
       obj.on().should.eql(3);
+      obj.on.reset();
+    });
+    
+    it('should allow multiple return values to be set', function() {
+      var obj = new EventEmitter;
+      obj.stub('on').andReturn(3, 2, 1);
+      obj.on().should.eql(3);
+      obj.on().should.eql(2);
+      obj.on().should.eql(1);
     });
   });
   
   describe('.reset()', function() {
     it('should reset the original method functionality', function() {
       var obj = new EventEmitter;
-      obj.foo = function() { 
-        return 4; 
-      }
+      obj.foo = foo;
       obj.stub('foo').andReturn(3);
       obj.foo().should.eql(3);
       obj.foo.reset();
       obj.foo().should.eql(4);
+    });
+  });
+  
+  describe('.once()', function() {
+    it('should should set expected calls to one.', function() {
+      var obj = new EventEmitter;
+      obj.foo = foo;
+      obj.stub('foo').it.should_be.called.once;
+      obj.foo();
+      obj.foo.reset();
+      
+      obj = new EventEmitter;
+      obj.foo = foo;
+      obj.stub('foo').it.should_be.called.once;
+      obj.foo();
+      obj.foo();
+      try {
+        obj.foo.reset();
+        throw new Error('Expected error.')
+      } catch (err) {}
     });
   });
 });
