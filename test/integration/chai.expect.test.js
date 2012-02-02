9fa1b2ef2609691a919e0928b3f87153d437c7f8
Jake Luer <jake.luer@gmail.com>
2012-02-02 01:17:15 -0500

    all tests for future tense counting

--- test/integration/chai.expect.test.js
+++ test/integration/chai.expect.test.js
@@ -45,65 +45,150 @@ describe('Chai expect() integration', function() {
   });
 
   describe('count', function () {
-    var myfoo = new Foo();
-    myfoo.stub('bar');
-    myfoo.stub('baz');
 
-    it('should work for once', function () {
-      myfoo.bar();
-      myfoo.baz();
-      myfoo.baz();
-      expect(myfoo.bar).to.have.been.called.once;
+    describe('past tense', function () {
+      var myfoo = new Foo();
+      myfoo.stub('bar');
+      myfoo.stub('baz');
 
-      err(function () {
-        expect(myfoo.bar).to.not.have.been.called.once;
-      }, "expected method 'bar' to not have been called once");
+      it('should work for once', function () {
+        myfoo.bar();
+        myfoo.baz();
+        myfoo.baz();
+        expect(myfoo.bar).to.have.been.called.once;
 
-      err(function () {
-        expect(myfoo.baz).to.have.been.called.once;
-      }, "expected method 'baz' to have been called once but got 2");
+        err(function () {
+          expect(myfoo.bar).to.not.have.been.called.once;
+        }, "expected method 'bar' to not have been called once");
 
+        err(function () {
+          expect(myfoo.baz).to.have.been.called.once;
+        }, "expected method 'baz' to have been called once but got 2");
+
+      });
+
+      it('should work for twice', function () {
+        myfoo.bar();
+        myfoo.baz();
+        expect(myfoo.bar).to.have.been.called.twice;
+        err(function () {
+          expect(myfoo.baz).to.have.been.called.twice;
+        }, "expected method 'baz' to have been called twice but got 3");
+
+      });
+
+      it('should work for exactly', function () {
+        myfoo.bar();
+        myfoo.baz();
+        expect(myfoo.bar).to.have.been.called.exactly(3);
+        err(function () {
+          expect(myfoo.baz).to.have.been.called.exactly(3);
+        }, "expected method 'baz' to have been called 3 times but got 4");
+      });
+
+      it('should work for min', function () {
+        expect(myfoo.bar).to.have.been.called.min(3);
+        expect(myfoo.bar).to.have.been.called.min(2);
+        err(function () {
+          expect(myfoo.baz).to.have.been.called.min(5);
+        }, "expected method \'baz\' to have been called minimum of 5 times but got 4");
+        err(function () {
+          expect(myfoo.baz).to.have.been.not.called.min(2);
+        }, "expected method \'baz\' to have been called less than 2 times but got 4");
+      });
+
+      it('should work for max', function () {
+        expect(myfoo.bar).to.have.been.called.max(3);
+        expect(myfoo.bar).to.have.been.called.max(4);
+        err(function () {
+          expect(myfoo.baz).to.have.been.called.max(3);
+        }, "expected method \'baz\' to have been called maximum of 3 times but got 4");
+        err(function () {
+          expect(myfoo.baz).to.have.not.been.called.max(5);
+        }, "expected method \'baz\' to have been called more than 5 times but got 4");
+      });
     });
 
-    it('should work for twice', function () {
-      myfoo.bar();
-      myfoo.baz();
-      expect(myfoo.bar).to.have.been.called.twice;
-      err(function () {
-        expect(myfoo.baz).to.have.been.called.twice;
-      }, "expected method 'baz' to have been called twice but got 3");
+    describe('future tense', function () {
+      var myfoo;
 
-    });
+      beforeEach(function () {
+        myfoo = new Foo();
+        myfoo.stub('bar');
+        myfoo.stub('baz');
+      });
 
-    it('should work for exactly', function () {
-      myfoo.bar();
-      myfoo.baz();
-      expect(myfoo.bar).to.have.been.called.exactly(3);
-      err(function () {
-        expect(myfoo.baz).to.have.been.called.exactly(3);
-      }, "expected method 'baz' to have been called 3 times but got 4");
-    });
+      it('should work for once', function () {
+        expect(myfoo.bar).to.be.called.once;
+        expect(myfoo.baz).to.be.called.once;
+        myfoo.bar();
+        expect(function () {
+          myfoo.bar.reset();
+        }).to.not.throw();
+        expect(function () {
+          myfoo.baz.reset();
+        }).to.throw();
+      });
 
-    it('should work for min', function () {
-      expect(myfoo.bar).to.have.been.called.min(3);
-      expect(myfoo.bar).to.have.been.called.min(2);
-      err(function () {
-        expect(myfoo.baz).to.have.been.called.min(5);
-      }, "expected method \'baz\' to have been called minimum of 5 times but got 4");
-      err(function () {
-        expect(myfoo.baz).to.have.been.not.called.min(2);
-      }, "expected method \'baz\' to have been called less than 2 times but got 4");
-    });
+      it('should work for twice', function () {
+        expect(myfoo.bar).to.be.called.twice;
+        expect(myfoo.baz).to.be.called.twice;
+        myfoo.bar();
+        myfoo.bar();
+        myfoo.baz();
+        expect(function () {
+          myfoo.bar.reset();
+        }).to.not.throw();
+        expect(function () {
+          myfoo.baz.reset();
+        }).to.throw();
+      });
 
-    it('should work for max', function () {
-      expect(myfoo.bar).to.have.been.called.max(3);
-      expect(myfoo.bar).to.have.been.called.max(4);
-      err(function () {
-        expect(myfoo.baz).to.have.been.called.max(3);
-      }, "expected method \'baz\' to have been called maximum of 3 times but got 4");
-      err(function () {
-        expect(myfoo.baz).to.have.not.been.called.max(5);
-      }, "expected method \'baz\' to have been called more than 5 times but got 4");
+      it('should work for exacly', function () {
+        expect(myfoo.bar).to.be.called.exactly(3);
+        expect(myfoo.baz).to.be.called.exactly(3);
+        myfoo.bar();
+        myfoo.bar();
+        myfoo.bar();
+        myfoo.baz();
+        myfoo.baz();
+        expect(function () {
+          myfoo.bar.reset();
+        }).to.not.throw();
+        expect(function () {
+          myfoo.baz.reset();
+        }).to.throw();
+      });
+
+      it('should work for min', function () {
+        expect(myfoo.bar).to.be.called.min(2);
+        expect(myfoo.baz).to.be.called.min(2);
+        myfoo.bar();
+        myfoo.bar();
+        myfoo.baz();
+        expect(function () {
+          myfoo.bar.reset();
+        }).to.not.throw();
+        expect(function () {
+          myfoo.baz.reset();
+        }).to.throw();
+      });
+
+      it('should work for max', function () {
+        expect(myfoo.bar).to.be.called.max(2);
+        expect(myfoo.baz).to.be.called.max(2);
+        myfoo.bar();
+        myfoo.bar();
+        myfoo.baz();
+        myfoo.baz();
+        myfoo.baz();
+        expect(function () {
+          myfoo.bar.reset();
+        }).to.not.throw();
+        expect(function () {
+          myfoo.baz.reset();
+        }).to.throw();
+      });
     });
   });
 });
