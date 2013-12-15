028dd57e9f2b5bd4c9dbc5207a28870b5c243cd9
Veselin Todorov <veselin@veselin.bg>
2013-12-15 17:22:29 +0200

    Add jack#revert

--- test/jack.js
+++ test/jack.js
@@ -2,13 +2,26 @@ describe('#stub', function() {
   it('stubs a method', function() {
     var foo = { bar: function() { return 3 } };
     jack.stub(foo, 'bar');
+
     assert(typeof foo.bar() === 'undefined');
+
+    foo.bar.revert();
+  });
+
+  it('can be configured what to return', function() {
+    var foo = { bar: function() { return 3 } };
+    jack.stub(foo, 'bar', { returns: 4 });
+
+    assert(foo.bar() === 4);
+
+    foo.bar.revert();
   });
 
   it('can reset a stubbed method', function() {
     var foo = { bar: function() { return 3 } };
     jack.stub(foo, 'bar');
     foo.bar.revert();
+
     assert(foo.bar() === 3);
   });
 });
