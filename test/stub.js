ee25e6e08820058f64e6820f5783b119cfec7940
Veselin Todorov <veselin@veselin.bg>
2013-12-15 17:43:13 +0200

    Extract tests

--- test/stub.js
+++ test/stub.js
@@ -25,19 +25,3 @@ describe('#stub', function() {
     assert(foo.bar() === 3);
   });
 });
-
-describe('#revert', function() {
-  it('resets all modified methods', function() {
-    var foo = { bar: function bar() { return 3 } };
-    var bar = { foo: function foo() { return 4 } };
-
-    jack.stub(foo, 'bar');
-    jack.stub(bar, 'foo');
-    jack.stub(bar, 'foo');
-
-    jack.revert();
-
-    assert(foo.bar() === 3);
-    assert(bar.foo() === 4);
-  });
-});
