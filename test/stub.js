730569078a291d68abca87664f9fbb3d0677013b
Veselin Todorov <veselin@veselin.bg>
2013-12-16 06:43:48 +0200

    Merge stubs and spies into doubles

--- test/stub.js
+++ test/stub.js
@@ -1,42 +1,17 @@
 describe('#stub', function() {
-  it('stubs a method', function() {
+  it('replaces the original method with provided fn', function() {
     var foo = { bar: function() { return 3 } };
-    jack.stub(foo, 'bar');
-    assert(typeof foo.bar() === 'undefined');
-    foo.bar.revert();
-  });
-
-  it('can be configured what to return', function() {
-    var foo = { bar: function() { return 3 } };
-    jack.stub(foo, 'bar', { returns: 4 });
+    jack.stub(foo, 'bar', function() {
+      return 4;
+    });
     assert(foo.bar() === 4);
-    foo.bar.revert();
   });
 
   it('can reset a stubbed method', function() {
     var foo = { bar: function() { return 3 } };
-    jack.stub(foo, 'bar');
+    jack.stub(foo, 'bar', function() {});
     foo.bar.revert();
-
     assert(foo.bar() === 3);
-  });
-
-  it('can be configured to throw an error', function() {
-    var foo = { bar: function() { return 3 } };
-    var err = null;
-
-    jack.stub(foo, 'bar', {
-      throws: new Error('test')
-    });
-
-    try {
-      foo.bar();
-    } catch(e) {
-      err = e;
-    }
-
-    assert(err);
-    jack.revert();
   });
 
   it('matches the number of args of the original', function() {
@@ -56,13 +31,5 @@ describe('#stub', function() {
     for (var i = 1, len = 11; i < len; i++) {
       assert(jack.stub(obj, 'fn' + i ).length === i, i);
     }
-  });
-
-  it('can use a custom function', function() {
-    var foo = { bar: function() { return 3 } };
-    jack.stub(foo, 'bar', function() {
-      return 4;
-    });
-    assert(foo.bar() === 4);
   });
 });
